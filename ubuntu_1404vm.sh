c9f748e11dcf27acb3b7d0ec851bcda104e83dcd
mcko <jkw552403@gmail.com>
2016-11-22 16:06:13 +0800

    install pyenv

--- ubuntu_1404vm.sh
+++ ubuntu_1404vm.sh
@@ -3,6 +3,7 @@ sudo apt-get install git
 
 #zsh
 sudo apt-get install zsh
+cp zsh/.zshrc ~/
 
 #ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -14,3 +15,7 @@ sudo apt-get install htop
 cp tmux/.tmux18.conf ~/.tmux.conf
 
 #vim
+
+#python
+curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
