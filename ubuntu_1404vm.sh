85ec63806c3c499d5c5fa979c6e8f917955d33a3
mcko <jkw552403@gmail.com>
2016-11-22 15:27:08 +0800

    basic ubuntu14 setting

---
+++ ubuntu_1404vm.sh
@@ -0,0 +1,16 @@
+#git
+sudo apt-get install git
+
+#zsh
+sudo apt-get install zsh
+
+#ohmyzsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#htop
+sudo apt-get install htop
+
+#tmux setting
+cp tmux/.tmux18.conf
+
+#vim
