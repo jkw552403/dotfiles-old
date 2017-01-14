c7ab833ff3bcca06191520dcf174ca1a22954a35
mcko <jkw552403@gmail.com>
2017-01-14 13:39:17 +0800

    Add IPython configuration

--- ubuntu_1404vm.sh
+++ ubuntu_1404vm.sh
@@ -23,3 +23,6 @@ cp tmux/.tmux18.conf ~/.tmux.conf
 #python
 curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+#ipython
+cp ipython/ipython_config.py ~/.ipython/profile_default/ipython_config.py
